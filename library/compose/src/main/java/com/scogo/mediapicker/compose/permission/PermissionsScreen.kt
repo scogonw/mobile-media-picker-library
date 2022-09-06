2520e9b8b2d4c6b8298c018f6c4db7ee346ced80
mukul372 <mukul@scogo.in>
2022-09-06 18:24:22 +0530

    permissions v1

---
+++ library/compose/src/main/java/com/scogo/mediapicker/compose/permission/PermissionsScreen.kt
@@ -0,0 +1,74 @@
+package com.scogo.mediapicker.compose.permission
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.Text
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+
+//import com.google.accompanist.permissions.ExperimentalPermissionsApi
+//import com.google.accompanist.permissions.rememberMultiplePermissionsState
+
+//@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun PermissionsScreen(
+    modifier: Modifier = Modifier,
+    scaffoldState: ScaffoldState = rememberScaffoldState(),
+    permissions: List<String>,
+    navigateToHome: () -> Unit,
+) {
+//    val permissionsState = rememberMultiplePermissionsState(permissions = permissions)
+//    if(permissionsState.allPermissionsGranted) {
+//        navigateToHome()
+//    }else {
+//        PermissionsView(
+//            modifier = modifier,
+//            scaffoldState = scaffoldState,
+//            navigateToHome = navigateToHome
+//        )
+//    }
+}
+
+@Composable
+internal fun PermissionsView(
+    modifier: Modifier,
+    scaffoldState: ScaffoldState,
+    navigateToHome: () -> Unit,
+){
+    val permissionsDenied = remember {
+        mutableStateOf(false)
+    }
+    Scaffold(
+        scaffoldState = scaffoldState,
+        content = {
+            Column(
+                modifier = modifier.fillMaxSize()
+            ) {
+
+            }
+        }
+    )
+}
+
+@Preview
+@Composable
+internal fun PermissionsPreview() {
+    Text(text = "hello")
+}
+
+
+
+
+
+
+
+
+
+
+
