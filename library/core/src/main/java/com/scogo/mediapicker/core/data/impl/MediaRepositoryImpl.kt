a522c509667cb97a47786f185999bfd6a01c3b82
mukul372 <mukul@scogo.in>
2022-09-09 13:16:53 +0530

    fetch videos

--- library/core/src/main/java/com/scogo/mediapicker/core/data/impl/MediaRepositoryImpl.kt
+++ library/core/src/main/java/com/scogo/mediapicker/core/data/impl/MediaRepositoryImpl.kt
@@ -5,27 +5,27 @@ import androidx.paging.PagingSource
 import com.scogo.mediapicker.core.data.api.MediaDataSource
 import com.scogo.mediapicker.core.data.api.MediaRepository
 import com.scogo.mediapicker.core.media.MediaData
-import com.scogo.mediapicker.core.media.fetchPictures
-import com.scogo.mediapicker.utils.createCursor
+import com.scogo.mediapicker.core.media.createMediaCursor
+import com.scogo.mediapicker.core.media.fetchMedia
 
 class MediaRepositoryImpl(
     private val context: Context
 ): MediaRepository {
 
     override suspend fun getCount(): Int {
-        val cursor = context.createCursor(Int.MAX_VALUE,0) ?: return 0
+        val cursor = context.createMediaCursor(Int.MAX_VALUE,0) ?: return 0
         val count = cursor.count
         cursor.close()
         return count
     }
 
     override suspend fun getByOffset(offset: Int): MediaData? {
-        return context.fetchPictures(1,offset).firstOrNull()
+        return context.fetchMedia(1,offset).firstOrNull()
     }
 
     override fun getMediaPagingSource(): PagingSource<Int, MediaData> {
         return MediaDataSource { limit, offset ->
-            context.fetchPictures(limit, offset)
+            context.fetchMedia(limit, offset)
         }
     }
 
