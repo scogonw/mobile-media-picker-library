e488ff424b9fabc42181eee484b78b6d19fee944
mukul372 <mukul@scogo.in>
2022-09-14 17:22:26 +0530

    initial commit

--- library/compose/src/main/java/com/scogo/mediapicker/compose/home/HomeActivity.kt
+++ library/compose/src/main/java/com/scogo/mediapicker/compose/home/HomeActivity.kt
@@ -14,6 +14,7 @@ import com.scogo.mediapicker.utils.Consts.WORK_ID
 import com.scogo.mediapicker.utils.getScogoMediaDirectory
 import com.scogo.mediapicker.utils.isPermissionsGranted
 
+
 internal class HomeActivity : ComponentActivity() {
 
     val mediaViewModel: MediaViewModel by viewModels {
