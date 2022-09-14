ad105ed384083653e62fcdcf7cfcb2817b1395bd
mukul372 <mukul@scogo.in>
2022-09-14 17:21:54 +0530

    bottom action bar

---
+++ library/common/ui-components/src/main/java/com/scogo/mediapicker/common/ui/components/custom/RoundedButton.kt
@@ -0,0 +1,43 @@
+package com.scogo.mediapicker.common.ui.components.custom
+
+import androidx.annotation.StringRes
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.ripple.rememberRipple
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
+import com.scogo.mediapicker.common.ui_theme.Dimens
+
+@Composable
+fun RoundedButton(
+    modifier: Modifier,
+    @StringRes actionName: Int,
+    onActionClick: () -> Unit = {}
+) {
+    Button(
+        modifier = modifier.clickable(
+            interactionSource = MutableInteractionSource(),
+            indication = rememberRipple(
+                bounded = true,
+                radius = Dp.Unspecified,
+                color = Color.Unspecified
+            )
+        ) {},
+        shape = RoundedCornerShape(Dimens.Seven),
+        onClick = onActionClick
+    ) {
+        Text(
+            text = stringResource(id = actionName),
+            style = MaterialTheme.typography.caption,
+            fontWeight = FontWeight.Bold,
+        )
+    }
+}
