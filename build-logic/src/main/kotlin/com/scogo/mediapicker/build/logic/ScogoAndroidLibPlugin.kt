d3ba5695b6f844d4e9a289f227fa41790e64d0c0
mukul372 <mukul@scogo.in>
2022-09-21 18:42:21 +0530

    release v1.0.2

--- build-logic/src/main/kotlin/com/scogo/mediapicker/build/logic/ScogoAndroidLibPlugin.kt
+++ build-logic/src/main/kotlin/com/scogo/mediapicker/build/logic/ScogoAndroidLibPlugin.kt
@@ -7,12 +7,17 @@ import com.android.build.gradle.LibraryExtension
 import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+import org.gradle.kotlin.dsl.create
+import org.gradle.kotlin.dsl.get
 
 @Suppress("UnstableApiUsage")
 class ScogoAndroidLibPlugin : Plugin<Project> {
     override fun apply(target: Project) {
         target.plugins.apply("com.android.library")
         target.plugins.apply("org.jetbrains.kotlin.android")
+        target.plugins.apply("maven-publish")
 
         target.extensions.getByType(LibraryExtension::class.java).also {
             it.compileSdk = target.ANDROID_COMPILE_SDK_VERSION
@@ -25,6 +30,27 @@ class ScogoAndroidLibPlugin : Plugin<Project> {
                 targetSdk = target.ANDROID_TARGET_SDK_VERSION
                 multiDexEnabled = true
                 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+            }
+            it.publishing {
+                singleVariant("release") {
+                    withSourcesJar()
+                    withJavadocJar()
+                }
+            }
+        }
+
+        target.afterEvaluate {
+            target.extensions.getByType(PublishingExtension::class.java).also {
+                it.publications.also { pub ->
+                    pub.create<MavenPublication>("maven").also { maven ->
+                        with(maven) {
+                            groupId = "com.gitlab.scogo"
+                            artifactId = "scogo_media_picker_library"
+                            version = "1.0.2"
+                            from(components["release"])
+                        }
+                    }
+                }
             }
         }
     }
