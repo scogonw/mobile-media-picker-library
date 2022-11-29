8c39550ac62f40f595e71c57563db164e5e42acc
mukul372 <mukul@scogo.in>
2022-11-29 12:33:55 +0530

    storage permissions updated

--- library/compose/src/main/java/com/scogo/mediapicker/compose/home/HomeActivity.kt
+++ library/compose/src/main/java/com/scogo/mediapicker/compose/home/HomeActivity.kt
@@ -1,6 +1,5 @@
 package com.scogo.mediapicker.compose.home
 
-import android.Manifest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -11,6 +10,7 @@ import com.scogo.mediapicker.compose.media.MediaViewModelFactory
 import com.scogo.mediapicker.compose.navigation.AppNavigationParams
 import com.scogo.mediapicker.core.data.impl.MediaRepositoryImpl
 import com.scogo.mediapicker.core.event.PushEvent
+import com.scogo.mediapicker.core.permission.AppPermissionManager
 import com.scogo.mediapicker.utils.Consts.WORK_ID
 import com.scogo.mediapicker.utils.getScogoMediaDirectory
 import com.scogo.mediapicker.utils.isPermissionsGranted
@@ -20,22 +20,19 @@ import org.greenrobot.eventbus.ThreadMode
 
 internal class HomeActivity : ComponentActivity() {
 
+    private var manager: AppPermissionManager? = null
+
     val mediaViewModel: MediaViewModel by viewModels {
         MediaViewModelFactory(MediaRepositoryImpl(this))
     }
 
-    private val permissions by lazy {
-        listOf(
-            Manifest.permission.CAMERA,
-            Manifest.permission.RECORD_AUDIO,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        )
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        manager = AppPermissionManager.register(this, this)
+        val permissions = manager?.requiredAppPermissions() ?: emptyList()
+
         val workId = intent.extras?.getString(WORK_ID)
-        if(!mediaViewModel.initRequestData(workId)) finish()
+        if (!mediaViewModel.initRequestData(workId)) finish()
 
         val appNavParams = AppNavigationParams(
             permissions = permissions,
@@ -53,21 +50,21 @@ internal class HomeActivity : ComponentActivity() {
 
     @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
     fun onEvent(event: PushEvent) {
-        if(event.workId == mediaViewModel.readRequestData().readId()) {
+        if (event.workId == mediaViewModel.readRequestData().readId()) {
             finish()
         }
     }
 
     override fun onStart() {
         super.onStart()
-        if(!EventBus.getDefault().isRegistered(this)) {
+        if (!EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().register(this)
         }
     }
 
     override fun onStop() {
         super.onStop()
-        if(EventBus.getDefault().isRegistered(this)) {
+        if (EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().unregister(this)
         }
     }
---
+++ library/core/src/main/java/com/scogo/mediapicker/core/permission/AppPermissionManager.kt
@@ -0,0 +1,125 @@
+package com.scogo.mediapicker.core.permission
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
+
+interface AppPermissionManagerCallbacks {
+    fun checkAndRequestAppPermissions()
+    fun dispose()
+}
+
+class AppPermissionManager private constructor(
+    private var context: Activity?,
+    private var owner: LifecycleOwner?,
+) : AppPermissionManagerCallbacks, LifecycleObserver {
+
+    companion object {
+        const val REQUEST_APP_PERMISSIONS_CODE = 99
+
+        fun register(context: Activity?, owner: LifecycleOwner?): AppPermissionManager {
+            val manager = AppPermissionManager(
+                context = context, owner = owner
+            )
+            owner?.lifecycle?.addObserver(manager)
+            return manager
+        }
+    }
+
+    private val commonAppPermissions = listOf(
+        Manifest.permission.CAMERA,
+        Manifest.permission.RECORD_AUDIO
+    )
+
+    /**
+     * READ_MEDIA_IMAGES and READ_MEDIA_VIDEO
+     * required only for android 13 or TIRAMISU (api level 33)
+     * if app want to access other apps media file
+     */
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    private val readMediaPermissions = listOf(
+        Manifest.permission.READ_MEDIA_IMAGES,
+        Manifest.permission.READ_MEDIA_VIDEO,
+    )
+
+    /**
+     * READ_EXTERNAL_STORAGE
+     * required only below android 13 or TIRAMISU (max api level 32)
+     * if app want to access other apps media files
+     */
+    private val readExternalStoragePermission: String
+        get() {
+            return Manifest.permission.READ_EXTERNAL_STORAGE
+        }
+
+    /**
+     * WRITE_EXTERNAL_STORAGE
+     * required only below android 10 OR Q (max api level 29)
+     */
+    private val writeExternalStoragePermission: String
+        get() {
+            return Manifest.permission.WRITE_EXTERNAL_STORAGE
+        }
+
+    private val appPermissions: List<String>
+        get() {
+            return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                commonAppPermissions + readMediaPermissions
+            } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                commonAppPermissions + readExternalStoragePermission
+            } else {
+                commonAppPermissions + readExternalStoragePermission + writeExternalStoragePermission
+            }
+        }
+
+    fun requiredAppPermissions() = appPermissions
+
+    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+    override fun dispose() {
+        context = null
+        owner?.lifecycle?.removeObserver(this)
+        owner = null
+    }
+
+    override fun checkAndRequestAppPermissions() {
+        if (!isPermissionsGranted(appPermissions)) {
+            requestPermissions(appPermissions)
+        }
+    }
+
+    private fun requestPermissions(permissions: List<String>): Boolean {
+        if (context == null) return false
+        ActivityCompat.requestPermissions(
+            context!!,
+            permissions.toTypedArray(),
+            REQUEST_APP_PERMISSIONS_CODE
+        )
+        return true
+    }
+
+    fun isAppPermissionsGranted(): Boolean {
+        if (context == null) return false
+        val allGranted = appPermissions.map { isPermissionGranted(it) }
+        return allGranted.all { it }
+    }
+
+    private fun isPermissionGranted(permission: String): Boolean {
+        if (context == null) return false
+        val granted = ContextCompat.checkSelfPermission(context!!, permission)
+        return granted == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun isPermissionsGranted(permissions: List<String>): Boolean {
+        if (context == null) return false
+        val allGranted = permissions.map { isPermissionGranted(it) }
+        return allGranted.all { it }
+    }
+}
