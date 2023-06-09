849a9896910c6cfc9418c35376b0b84554e55fb6
Mukul Jangir <mukul.jangir372@gmail.com>
2023-06-09 14:34:25 +0530

    minor fixes

--- media-picker/src/main/java/com/scogo/mediapicker/compose/presentation/home/HomeActivity.kt
+++ media-picker/src/main/java/com/scogo/mediapicker/compose/presentation/home/HomeActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.scogo.mediapicker.compose.core.data.impl.MediaRepositoryImpl
 import com.scogo.mediapicker.compose.core.event.PushEvent
 import com.scogo.mediapicker.compose.core.permission.AppPermissionManager
@@ -14,6 +15,7 @@ import com.scogo.mediapicker.compose.presentation.theme.ScogoTheme
 import com.scogo.mediapicker.compose.util.Consts.WORK_ID
 import com.scogo.mediapicker.compose.util.getScogoMediaDirectory
 import com.scogo.mediapicker.compose.util.isPermissionsGranted
+import kotlinx.coroutines.launch
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
@@ -24,35 +26,6 @@ internal class HomeActivity : ComponentActivity() {
 
     val mediaViewModel: MediaViewModel by viewModels {
         MediaViewModelFactory(MediaRepositoryImpl(this))
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        manager = AppPermissionManager.register(this, this)
-        val permissions = manager?.requiredAppPermissions() ?: emptyList()
-
-        val workId = intent.extras?.getString(WORK_ID)
-        if (!mediaViewModel.initRequestData(workId)) finish()
-
-        val appNavParams = AppNavigationParams(
-            permissions = permissions,
-            permissionsGranted = isPermissionsGranted(permissions),
-            scogoMediaDir = getScogoMediaDirectory()
-        )
-        setContent {
-            ScogoTheme {
-                HomeScreen(
-                    params = appNavParams
-                )
-            }
-        }
-    }
-
-    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
-    fun onEvent(event: PushEvent) {
-        if (event.workId == mediaViewModel.readRequestData().readId()) {
-            finish()
-        }
     }
 
     override fun onStart() {
@@ -66,6 +39,37 @@ internal class HomeActivity : ComponentActivity() {
         super.onStop()
         if (EventBus.getDefault().isRegistered(this)) {
             EventBus.getDefault().unregister(this)
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        manager = AppPermissionManager.register(this, this)
+        val permissions = manager?.requiredAppPermissions() ?: emptyList()
+
+        val workId = intent.extras?.getString(WORK_ID)
+        if (!mediaViewModel.initRequestData(workId)) finish()
+
+        lifecycleScope.launch {
+            val appNavParams = AppNavigationParams(
+                permissions = permissions,
+                permissionsGranted = isPermissionsGranted(permissions),
+                scogoMediaDir = getScogoMediaDirectory()
+            )
+            setContent {
+                ScogoTheme {
+                    HomeScreen(
+                        params = appNavParams
+                    )
+                }
+            }
+        }
+    }
+
+    @Subscribe(sticky = true, threadMode = ThreadMode.MAIN)
+    fun onEvent(event: PushEvent) {
+        if (event.workId == mediaViewModel.readRequestData().readId()) {
+            finish()
         }
     }
 }
--- media-picker/src/main/java/com/scogo/mediapicker/compose/util/FileExts.kt
+++ media-picker/src/main/java/com/scogo/mediapicker/compose/util/FileExts.kt
@@ -1,27 +1,26 @@
 package com.scogo.mediapicker.compose.util
 
 import android.app.Activity
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 import java.io.File
 
-internal fun Activity.getScogoMediaDirectory(): File {
+internal suspend fun Activity.getScogoMediaDirectory(): File {
     return getMediaDirectory("Scogo")
 }
-internal fun Activity.getMediaDirectory(
-    name: String,
-): File {
+
+internal suspend fun Activity.getMediaDirectory(name: String): File = withContext(Dispatchers.IO) {
     val mediaDir = externalMediaDirs.firstOrNull()?.let {
-        File(it,name).apply {
-            if(!it.exists()) {
+        File(it, name).apply {
+            if (!it.exists()) {
                 mkdirs()
             }
         }
     }
-    return if(mediaDir != null && mediaDir.exists()) {
-        mediaDir
-    }else {
-        filesDir
-    }
+    if (mediaDir != null && mediaDir.exists()) mediaDir
+    else filesDir
 }
+
 internal fun String?.isVideo(): Boolean {
     return (this ?: "").contains("video")
 }
